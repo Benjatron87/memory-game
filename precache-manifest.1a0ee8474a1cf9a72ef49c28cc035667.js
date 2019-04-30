9e85e7460e920bf8a88ec0c7fba491cce7177d39
Benjatron <benjt122@gmail.com>
2019-04-30 15:52:25 -0400

    Updates

---
+++ precache-manifest.1a0ee8474a1cf9a72ef49c28cc035667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b68fe102bd0e2e599442522296f5490",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "b5390178185c1accda82",
+    "url": "/memory-game/static/css/main.d95d8cb4.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b5390178185c1accda82",
+    "url": "/memory-game/static/js/main.eebc33ad.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
